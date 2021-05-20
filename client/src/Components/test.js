8d40afe584ffac829bde39bca9e8272ff63437db
David Alteresco <dalteresco@aligntech.com>
2021-05-20 09:43:24 +0300

    with axios

--- client/src/Components/test.js
+++ client/src/Components/test.js
@@ -1,13 +1,23 @@
 import wikiPage from 'wiki-page';
 import { wiki } from '../stores';
+import Axios from "axios";
 
-
-const wikiFetch = () => {wikiPage.fetch({
+/* const wikiFetch = () => {wikiPage.fetch({
     section: 'page',
     type: 'html',
     title: 'Pony',
     }, (data) => {;
         wiki.WikiHtml = data;
-})};
+})}; */
+
+const wikiFetch = () => {
+    Axios.get(`https://en.wikipedia.org/w/api.php?action=parse&format=json&page=pony&prop=text`,{
+/*         username: username, 
+        password: password */
+    }).then(function (response) {
+        // handle success
+        wiki.wikiHtml = response;
+    });
+}
 
 export default wikiFetch;
